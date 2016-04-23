c342ff47162d3a31699e766dbc13635dbe83a4d3
rojoangel <rojo.angel@gmail.com>
2016-04-24 00:19:56 +0200

    add missing Direction class

---
+++ src/Direction.java
@@ -0,0 +1,11 @@
+public class Direction {
+
+    private final String directionAsString;
+
+    /**
+     * @param directionAsString String
+     */
+    public Direction(String directionAsString) {
+        this.directionAsString = directionAsString;
+    }
+}
