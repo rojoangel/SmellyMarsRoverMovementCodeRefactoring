c804a83facff8cfd4ae025e52a6a594aa38d6821
rojoangel <rojo.angel@gmail.com>
2016-04-24 23:09:49 +0200

    extract individual command creation to its own method

--- src/com/kata/mars/rover/Commands.java
+++ src/com/kata/mars/rover/Commands.java
@@ -26,18 +26,26 @@ public class Commands {
     public static Commands extractCommandsFrom(String commandsSequence) {
         List<Command> commands = new ArrayList<>();
         for (int i = 0; i < commandsSequence.length(); ++i) {
-            String command = commandsSequence.substring(i, i + 1);
-            if (command.equals("l")) {
-                commands.add(new RotateLeft());
-            } else if (command.equals("r")) {
-                commands.add(new RotateRight());
-            } else  if (command.equals("f")) {
-                commands.add(new MoveForward());
-            } else {
-                commands.add(new MoveBackward());
-            }
+            String commandRepresentation = commandsSequence.substring(i, i + 1);
+            commands.add(createCommand(commandRepresentation));
         }
         return new Commands(commands);
+    }
+
+    /**
+     * @param commandRepresentation String
+     * @return Command
+     */
+    private static Command createCommand(String commandRepresentation) {
+        if (commandRepresentation.equals("l")) {
+            return new RotateLeft();
+        } else if (commandRepresentation.equals("r")) {
+            return new RotateRight();
+        } else  if (commandRepresentation.equals("f")) {
+            return new MoveForward();
+        } else {
+            return new MoveBackward();
+        }
     }
 
     /**
