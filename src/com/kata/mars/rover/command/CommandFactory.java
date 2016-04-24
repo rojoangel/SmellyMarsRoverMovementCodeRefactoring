56fbdb27913e5deb7c4b28e4e22f1eba5a2296e0
rojoangel <rojo.angel@gmail.com>
2016-04-24 23:38:26 +0200

    replace switch in command factory with map initialization

--- src/com/kata/mars/rover/command/CommandFactory.java
+++ src/com/kata/mars/rover/command/CommandFactory.java
@@ -5,15 +5,13 @@ import com.kata.mars.rover.Command;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.sun.webkit.event.WCFocusEvent.BACKWARD;
-
 public class CommandFactory {
 
     private static final String LEFT = "l";
     private static final String RIGHT = "r";
     private static final String FORWARD = "f";
     private static final String BACKWARD = "b";
-    
+
     private static Map<String, Command> registeredCommands = registerCommands();
 
     /**
