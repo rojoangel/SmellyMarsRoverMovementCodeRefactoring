27454934c0f039d180705a742f3c7ad0f1b97ff8
rojoangel <rojo.angel@gmail.com>
2016-04-24 23:35:50 +0200

    replace switch in command factory with map initialization

--- src/com/kata/mars/rover/command/CommandFactory.java
+++ src/com/kata/mars/rover/command/CommandFactory.java
@@ -2,6 +2,9 @@ package com.kata.mars.rover.command;
 
 import com.kata.mars.rover.Command;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.sun.webkit.event.WCFocusEvent.BACKWARD;
 
 public class CommandFactory {
@@ -10,23 +13,29 @@ public class CommandFactory {
     private static final String RIGHT = "r";
     private static final String FORWARD = "f";
     private static final String BACKWARD = "b";
+    
+    private static Map<String, Command> registeredCommands = registerCommands();
+
+    /**
+     * @return Map<String, Command>
+     */
+    private static Map<String, Command> registerCommands() {
+        Map<String, Command> commands = new HashMap<>();
+        commands.put(LEFT, new RotateLeft());
+        commands.put(RIGHT, new RotateRight());
+        commands.put(FORWARD, new MoveForward());
+        commands.put(BACKWARD, new MoveBackward());
+        return commands;
+    }
 
     /**
      * @param representation String
      * @return Command
      */
     public static Command createCommand(String representation) {
-        switch (representation) {
-            case LEFT:
-                return new RotateLeft();
-            case RIGHT:
-                return new RotateRight();
-            case FORWARD:
-                return new MoveForward();
-            case BACKWARD:
-                return new MoveBackward();
-            default:
-                return new NullCommand();
+        if (!registeredCommands.containsKey(representation)) {
+            return new NullCommand();
         }
+        return registeredCommands.get(representation);
     }
 }
