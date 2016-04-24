4ff609aa2bafc0fa712392107f90dab6a263038f
rojoangel <rojo.angel@gmail.com>
2016-04-24 21:25:08 +0200

    add equality methods to Vector

--- src/com/kata/mars/rover/Vector.java
+++ src/com/kata/mars/rover/Vector.java
@@ -12,4 +12,23 @@ public class Vector {
         this.coordinates = coordinates;
         this.direction = direction;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vector vector = (Vector) o;
+
+        if (coordinates != null ? !coordinates.equals(vector.coordinates) : vector.coordinates != null) return false;
+        return direction != null ? direction.equals(vector.direction) : vector.direction == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = coordinates != null ? coordinates.hashCode() : 0;
+        result = 31 * result + (direction != null ? direction.hashCode() : 0);
+        return result;
+    }
 }
